08fb2fbbb87a689d2cab22d83111d871a7bc412d
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-06-15 00:39:08 +0600

    Design Update Only

--- gymMembership/src/app/components/charts/membership-chart/membership-chart.component.ts
+++ gymMembership/src/app/components/charts/membership-chart/membership-chart.component.ts
@@ -1,23 +1,40 @@
 import { Component, Signal } from '@angular/core';
 import { MemberService } from '../../services/member.service';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-membership-chart',
   templateUrl: './membership-chart.component.html',
-  styleUrl: './membership-chart.component.css'
+  styleUrl: './membership-chart.component.css',
 })
 export class MembershipChartComponent {
-
   membersByType: Signal<any[]>;
   monthlyRevenue: Signal<any[]>;
 
   // Chart options
   view: [number, number] = [0, 300]; // [width, height], 0 makes it responsive
-  barChartColorScheme = { domain: ['#cd7f32', '#a7a7a7', '#ffc400', '#e5e4e2'] };
+  barChartColorScheme = {
+    domain: ['#cd7f32', '#a7a7a7', '#ffc400', '#e5e4e2'],
+  };
   lineChartColorScheme = { domain: ['#0d6efd'] };
 
   constructor(private memberService: MemberService) {
     this.membersByType = this.memberService.membersByType;
     this.monthlyRevenue = this.memberService.monthlyRevenue;
   }
+
+  // Define a vibrant, futuristic color scheme
+  chartColorScheme: Color = {
+    name: 'futuristic',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: [
+      '#00bfff', // Deep Sky Blue
+      '#1e90ff', // Dodger Blue
+      '#32cd32', // Lime Green
+      '#ff4500', // Orange Red
+      '#da70d6', // Orchid
+    ],
+  };
+
 }
